e3e101f901a00a23d3d2fae9ec5116c46b5772bc
Seif <156626116+seiftheman@users.noreply.github.com>
2026-06-25 17:23:00 +0300

    Update 0001_initial.py

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -4,11 +4,26 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     initial = True
 
+    dependencies = []
+
     operations = [
         migrations.CreateModel(
             name="Pack",
             fields=[
-                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                (
+                    "id",
+                    models.BigAutoField(
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                (
+                    "discord_id",
+                    models.BigIntegerField(
+                        unique=True,
+                        help_text="Discord user ID",
+                    ),
+                ),
             ],
-        )
+        ),
     ]
