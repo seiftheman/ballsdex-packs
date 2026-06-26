79658130e5bea4f1eb3a99086d050dc19b267583
Seif <seifeldeen.mahmoud.2009@gmail.com>
2026-06-27 01:15:12 +0300

    Update __init__.py

--- packs/package/__init__.py
+++ packs/package/__init__.py
@@ -1,10 +1,10 @@
 from typing import TYPE_CHECKING
 
-from .cog import Pack
+from .cog import Packs
 
 if TYPE_CHECKING:
     from ballsdex.core.bot import BallsDexBot
 
 
 async def setup(bot: "BallsDexBot") -> None:
-    await bot.add_cog(Pack(bot))
+    await bot.add_cog(Packs(bot))
