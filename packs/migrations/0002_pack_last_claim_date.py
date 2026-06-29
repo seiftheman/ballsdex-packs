350a2365297dc750b28b28ddaf4f304f803ef0b0
Seif <seifeldeen.mahmoud.2009@gmail.com>
2026-06-30 01:53:12 +0300

    Create 0002_pack_last_claim_date.py

---
+++ packs/migrations/0002_pack_last_claim_date.py
@@ -0,0 +1,19 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("packs", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="pack",
+            name="last_claim_date",
+            field=models.DateTimeField(
+                null=True,
+                blank=True,
+                help_text="When this pack was claimed (used for cooldown tracking).",
+            ),
+        ),
+    ]
