5b67dab99155cb2dfa552e9e7c2ffbedb8720fc4
Seif <seifeldeen.mahmoud.2009@gmail.com>
2026-07-17 23:12:26 +0300

    oajwojs;dl

--- packs/models.py
+++ packs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Pack(models.Model):
-    id = models.BigAutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True, serialize=False)
     type = models.CharField(max_length=100, help_text="Type of the pack.")
     min_rarity = models.FloatField(
         null=True,
