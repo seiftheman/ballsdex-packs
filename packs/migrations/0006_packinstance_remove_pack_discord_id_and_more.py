bbd2e8e52725ef4e9c54db648025d511cb68d63e
Seif <seifeldeen.mahmoud.2009@gmail.com>
2026-07-18 20:55:46 +0300

    xjgopewojpgweojpg

--- packs/migrations/0006_packinstance_remove_pack_discord_id_and_more.py
+++ packs/migrations/0006_packinstance_remove_pack_discord_id_and_more.py
@@ -7,23 +7,17 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.DeleteModel(
-            name="PackInstance",
-        ),
-        migrations.AddField(
-            model_name="pack",
-            name="discord_id",
-            field=models.BigIntegerField(null=True),
-        ),
-        migrations.AddField(
-            model_name="pack",
-            name="is_opened",
-            field=models.BooleanField(default=False),
-        ),
-        migrations.AddField(
-            model_name="pack",
-            name="last_claim_date",
-            field=models.DateTimeField(blank=True, null=True),
+        migrations.CreateModel(
+            name='PackInstance',
+            fields=[
+                ('id', models.BigAutoField(primary_key=True, serialize=False)),
+                ('type', models.CharField(help_text='Type of the pack.', max_length=100)),
+                ('discord_id', models.BigIntegerField(help_text='Discord user ID.')),
+                ('last_claim_date', models.DateTimeField(blank=True, help_text='When this pack was claimed (used for cooldown tracking).', null=True)),
+                ('min_rarity', models.FloatField(help_text='Minimum rarity for balls in this pack.', null=True)),
+                ('max_rarity', models.FloatField(help_text='Maximum rarity for balls in this pack.', null=True)),
+                ('is_opened', models.BooleanField(default=False, help_text='Whether this pack has already been opened.')),
+            ],
         ),
         migrations.RenameModel(
             old_name="Pack",
