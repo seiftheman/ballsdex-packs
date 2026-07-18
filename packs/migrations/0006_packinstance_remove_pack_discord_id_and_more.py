4a59a844716b9ac327693ba75b95645e61a77d33
Seif <seifeldeen.mahmoud.2009@gmail.com>
2026-07-18 19:40:30 +0300

    Update 0006_packinstance_remove_pack_discord_id_and_more.py

--- packs/migrations/0006_packinstance_remove_pack_discord_id_and_more.py
+++ packs/migrations/0006_packinstance_remove_pack_discord_id_and_more.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("packs", "0005_pack_max_rarity_pack_min_rarity"),
+        ("pack", "0005_pack_max_rarity_pack_min_rarity"),
     ]
 
     operations = [
