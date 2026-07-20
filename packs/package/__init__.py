b85c1e4745a1e8217913056f45158b8b0f2766c6
Seif <seifeldeen.mahmoud.2009@gmail.com>
2026-07-20 18:45:16 +0300

    jorgejoogjwegojpweojpgwe

--- packs/package/__init__.py
+++ packs/package/__init__.py
@@ -1,15 +1,12 @@
 from typing import TYPE_CHECKING
-
+from packs.models import Pack
 from .cog import PackCog
-from packs.models import Pack, PackInstance
 
 if TYPE_CHECKING:
     from ballsdex.core.bot import BallsDexBot
 
 
 async def setup(bot: "BallsDexBot") -> None:
-    await Pack.objects.aget_or_create(name="Daily", type="daily")
-    await Pack.objects.aget_or_create(name="Weekly", type="weekly")
-    await Pack.objects.aget_or_create(type="daily").aupdate(name="Daily")
-    await Pack.objects.aget_or_create(type="weekly").aupdate(name="Weekly")
+    await Pack.objects.aupdate_or_create(type="daily", defaults={"name": "Daily"})  
+    await Pack.objects.aupdate_or_create(type="weekly", defaults={"name": "Weekly"})
     await bot.add_cog(PackCog(bot))
