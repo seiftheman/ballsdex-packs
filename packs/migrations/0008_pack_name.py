0cf7dd90b42980f40a9e0b80d6247356698dfb3a
Seif <seifeldeen.mahmoud.2009@gmail.com>
2026-07-20 19:27:14 +0300

    Hi.

--- packs/migrations/0008_pack_name.py
+++ packs/migrations/0008_pack_name.py
@@ -8,9 +8,27 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='pack',
-            name='name',
-            field=models.CharField(default='Unnamed Pack', help_text='Name of the pack.', max_length=100, unique=True),
-        ),
+        migrations.SeparateDatabaseAndState(
+            database_operations=[
+                migrations.RunSQL(
+                    sql="""
+                    ALTER TABLE packs_pack 
+                    ADD COLUMN IF NOT EXISTS name varchar(100) DEFAULT 'Unnamed Pack' UNIQUE;
+                    """,
+                    reverse_sql="ALTER TABLE packs_pack DROP COLUMN IF EXISTS name;",
+                )
+            ],
+            state_operations=[
+                migrations.AddField(
+                    model_name='pack',
+                    name='name',
+                    field=models.CharField(
+                        default='Unnamed Pack',
+                        help_text='Name of the pack.',
+                        max_length=100,
+                        unique=True,
+                    ),
+                ),
+            ],
+        )
     ]
